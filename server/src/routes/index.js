436d09d47ff1c168877595c89ba7a5cad7f1f002
Valentin Minkov <minkowalentin@proton.me>
2023-04-24 21:51:23 +0200

    feat: add health check

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,7 +4,9 @@ const authRoutes = require("./auth");
 const spotifyRoutes = require("./spotify");
 
 const router = express.Router();
-
+router.get("/healthcheck", (req, res) => {
+  res.status(200).json({ status: "OK", message: "App is running smoothly" });
+});
 router.use("/auth", authRoutes);
 router.use("/spotify", spotifyRoutes);
 
