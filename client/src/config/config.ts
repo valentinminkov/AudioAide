db6e5f208b1251bac24ec26c5b2abba180313fb2
Valentin Minkov <minkowalentin@proton.me>
2023-04-29 01:30:50 +0200

    refactor: add spotify auth url in config

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,2 +1,6 @@
 // src/config/config.ts
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8888/api";
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8888/api";
+export const SPOTIFY_AUTH_URL =
+  process.env.REACT_APP_SPOTIFY_AUTH_URL ||
+  "http://localhost:8888/api/auth/login";
