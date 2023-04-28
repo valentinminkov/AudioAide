834abfb1b0c2bbc745090590bb44a7ee7b736666
Valentin Minkov <minkowalentin@proton.me>
2023-04-28 23:02:17 +0200

    feat: fetch the users access_token and refresh_token from redis by user's id in the middleware

--- server/src/middleware/spotifyAccessToken.js
+++ server/src/middleware/spotifyAccessToken.js
@@ -1,6 +1,7 @@
 // path: app/src/middleware/spotifyAccessToken.js
 const SpotifyWebApi = require("spotify-web-api-node");
 const { clientId, clientSecret } = require("../config");
+const redisClient = require("../redisClient");
 
 const spotifyApi = new SpotifyWebApi({
   clientId: clientId,
@@ -8,16 +9,67 @@ const spotifyApi = new SpotifyWebApi({
 });
 
 // Middleware function to set the access token
-function setSpotifyAccessToken(req, res, next) {
-  const access_token = req.session.access_token;
-  if (!access_token) {
-    res.status(401).json({ error: "Unauthorized" });
-    return;
-  }
+async function setSpotifyAccessToken(req, res, next) {
+  const userId = req.params.userId;
 
-  spotifyApi.setAccessToken(access_token);
-  req.spotifyApi = spotifyApi; // Pass the spotifyApi instance to the controllers
-  next();
+  try {
+    const access_token = await redisClient.hGet(
+      `user:${userId}`,
+      "access_token"
+    );
+    const refresh_token = await redisClient.hGet(
+      `user:${userId}`,
+      "refresh_token"
+    );
+
+    if (!access_token || !refresh_token) {
+      res
+        .status(401)
+        .json({ error: "Unauthorized. Missing acces_token or refresh_token" });
+      return;
+    }
+
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
+
+    try {
+      await spotifyApi.getMe();
+    } catch (err) {
+      if (err.statusCode === 401) {
+        try {
+          await spotifyApi.refreshAccessToken();
+          const newAccessToken = spotifyApi.getAccessToken();
+
+          await redisClient.hSet(
+            `user:${userId}`,
+            "access_token",
+            newAccessToken
+          );
+
+          spotifyApi.setAccessToken(newAccessToken);
+
+          try {
+            await spotifyApi.getMe(); // Check again with the new access token
+          } catch (secondError) {
+            res.status(401).json({ error: "Unauthorized new access_token." });
+            return;
+          }
+        } catch (refreshError) {
+          res.status(401).json({ error: "Unable to refresh access token" });
+          return;
+        }
+      } else {
+        console.log("Error getting user data:", err);
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+      }
+    }
+
+    req.spotifyApi = spotifyApi;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Unauthorized" });
+  }
 }
 
 module.exports = setSpotifyAccessToken;
