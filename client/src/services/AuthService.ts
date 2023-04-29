53965bb7edf50511b6f9877fa2b13ccad478aeac
Valentin Minkov <minkowalentin@proton.me>
2023-04-29 17:05:51 +0200

    refactor: remove authService as it is not used anymore

--- client/src/services/AuthService.ts
+++
@@ -1,9 +0,0 @@
-// src/services/AuthService.ts
-export const saveUserId = (userId: string) => {
-    localStorage.setItem("user_id", userId);
-  };
-  
-  export const getUserId = () => {
-    return localStorage.getItem("user_id");
-  };
-  
