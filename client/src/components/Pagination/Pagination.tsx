ec5b1418a516c5e1c7dedcce4c68ad15afa74607
Valentin Minkov <minkowalentin@proton.me>
2023-05-03 23:37:17 +0200

    feat: add basic pagination component

---
+++ client/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,35 @@
+// components/Pagination/Pagination.tsx
+import React from "react";
+import styles from "./Pagination.module.scss";
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  totalPages,
+  currentPage,
+  onPageChange,
+}) => {
+  const pages = Array.from(Array(totalPages).keys()).map((i) => i + 1);
+
+  return (
+    <div className={styles.pagination}>
+      {pages.map((page) => (
+        <button
+          key={page}
+          className={`${styles.page} ${
+            page === currentPage ? styles.active : ""
+          }`}
+          onClick={() => onPageChange(page)}
+        >
+          {page}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Pagination;
