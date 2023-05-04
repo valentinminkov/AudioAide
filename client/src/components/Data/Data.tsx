7b080e373768c23d261201b353da4c17d590e991
Valentin Minkov <minkowalentin@proton.me>
2023-05-04 23:42:41 +0200

    style: basic style for Data component

--- client/src/components/Data/Data.tsx
+++ client/src/components/Data/Data.tsx
@@ -1,4 +1,5 @@
 import { Image } from "../../interfaces/Spotify";
+import styles from "./Data.module.scss"
 
 interface Props {
   title: string;
@@ -8,10 +9,10 @@ interface Props {
 const Data = ({ title, images }: Props) => {
   const imageUrl = images?.length > 0 ? images[0]?.url : null;
   return (
-    <div>
+    <div className={styles.container}>
       <h4>{title}</h4>
       {imageUrl ? (
-        <img src={imageUrl} alt={title} />
+        <img src={imageUrl} alt={title}  className={styles.img}/>
       ) : (
         <p>No image available</p>
       )}
