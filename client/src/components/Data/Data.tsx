ddac25848ed7b9946e89342f199fcfd2a16d6cad
Valentin Minkov <minkowalentin@proton.me>
2023-05-05 21:36:08 +0200

    style: adjust data style

--- client/src/components/Data/Data.tsx
+++ client/src/components/Data/Data.tsx
@@ -1,5 +1,5 @@
 import { Image } from "../../interfaces/Spotify";
-import styles from "./Data.module.scss"
+import styles from "./Data.module.scss";
 
 interface Props {
   title: string;
@@ -10,12 +10,12 @@ const Data = ({ title, images }: Props) => {
   const imageUrl = images?.length > 0 ? images[0]?.url : null;
   return (
     <div className={styles.container}>
-      <h4>{title}</h4>
       {imageUrl ? (
-        <img src={imageUrl} alt={title}  className={styles.img}/>
+        <img src={imageUrl} alt={title} className={styles.img} />
       ) : (
         <p>No image available</p>
       )}
+      <span className={styles.title}>{title}</span>
     </div>
   );
 };
