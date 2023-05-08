84e58ce5dcd84ec7671b98d0bc0d1292b514ad39
Valentin Minkov <minkowalentin@proton.me>
2023-05-08 21:22:17 +0200

    refactor: adjust Playlist component to use components

--- client/src/pages/Library/Playlist/Playlist.tsx
+++ client/src/pages/Library/Playlist/Playlist.tsx
@@ -5,6 +5,8 @@ import { getPlaylist } from "../../../services/Spotify";
 import { PlaylistResponse } from "../../../interfaces/Spotify";
 import { AppContext } from "../../../context/AppContext";
 import Spinner from "../../../components/Spinner/Spinner";
+import List from "../../../components/List/List";
+import DescriptionCard from "../../../components/DescriptionCard/DescriptionCard";
 
 interface Props {}
 
@@ -28,36 +30,36 @@ const Playlist: React.FC<Props> = ({}) => {
     getPlaylistData();
   }, [id, user]);
 
+  const columnHeaders = ["Title", "Artist", "huehuehuehu"];
+  const listColumns = playlist?.tracks.items.map((item) => [
+    item.track.name,
+    item.track.artists.map((artist) => artist.name).join(", "),
+    <a href={item.track.external_urls.spotify}>Open Track</a>,
+  ]);
+  const image = playlist?.images[0];
+  const title = playlist?.name;
+  const descriptionRows = [
+    { title: "Owner", content: playlist?.owner.display_name || "" },
+    { title: "Followers", content: playlist?.followers.total.toString() || "" },
+    {
+      content: <a href={playlist?.external_urls.spotify}>Open in Spotify</a>,
+    },
+  ];
   return (
     <div className={style.container}>
       {playlist ? (
         <>
-          <h3>{playlist.name}</h3>
-          <img
-            src={playlist.images[0].url}
-            alt={playlist.name}
-            width={playlist.images[0].width}
-            height={playlist.images[0].height}
-          />
-          <p>Owner: {playlist.owner.display_name}</p>
-          <p>Followers: {playlist.followers.total}</p>
-          <p>
-            <a href={playlist.external_urls.spotify}>Open in Spotify</a>
-          </p>
+          {image && title && descriptionRows && (
+            <DescriptionCard
+              image={image}
+              title={title}
+              rows={descriptionRows}
+            />
+          )}
           <h4>Tracks</h4>
-          <ol>
-            {playlist.tracks.items.map((item) => (
-              <li key={item.track.id}>
-                <span>{item.track.name}</span> -{" "}
-                <span>
-                  {item.track.artists.map((artist) => artist.name).join(", ")}
-                </span>
-                <p>
-                  <a href={item.track.external_urls.spotify}>Open Track</a>
-                </p>
-              </li>
-            ))}
-          </ol>
+          {listColumns && (
+            <List columnHeaders={columnHeaders} columns={listColumns} />
+          )}
         </>
       ) : (
         <Spinner />
