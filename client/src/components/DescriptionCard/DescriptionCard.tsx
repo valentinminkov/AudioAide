8c28a4b1f68eb657ee9913b6b56a0cc7c7cba207
Valentin Minkov <minkowalentin@proton.me>
2023-05-08 21:20:59 +0200

    feat: add descriptionCard component

---
+++ client/src/components/DescriptionCard/DescriptionCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import style from "./DescriptionCard.module.scss";
+import { Image } from "../../interfaces/Spotify";
+import { v4 as uuidv4 } from "uuid";
+
+interface DescriptionRow {
+  title?: string;
+  content: string | JSX.Element;
+}
+
+interface Props {
+  image: Image;
+  title: string;
+  rows: DescriptionRow[];
+}
+
+const DescriptionCard: React.FC<Props> = ({ image, title, rows }) => {
+  const imgSize = 200;
+  return (
+    <div className={style.container}>
+      <div>
+        <img
+          src={image.url || ""}
+          alt={title}
+          width={imgSize}
+          height={imgSize}
+        />
+      </div>
+      <div>
+        <h3>{title}</h3>
+        {rows.map(({ title, content }) => (
+          <div key={uuidv4()} className={style.descriptionRow}>
+            {title && <span>{title}</span>}
+            <span>{content}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionCard;
